5135bb2ee06fe2650b365a2aed97491f29c3a01c
harunkaracobanibram <harun.karacoban@abisteknoloji.com.tr>
2024-05-15 17:31:08 +0300

    axios added and login request changed

--- reference-project-frontend/src/services/LoginPage/LoginPageRequests.ts
+++ reference-project-frontend/src/services/LoginPage/LoginPageRequests.ts
@@ -1,16 +1,10 @@
+import axios from "axios";
 import { ILoginModel } from "../../models/ILoginModel";
 
 export const sendLoginRequest = async (loginData: ILoginModel) => {
-  const requestOptions = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(loginData),
-  };
-  fetch(`${import.meta.env.VITE_API}/auth/login`, requestOptions).then(
-    (response) => {
-      return response.json();
-    }
+  const result = await axios.post(
+    `${import.meta.env.VITE_API}/auth/login`,
+    loginData
   );
+  return result;
 };
